00a0cf798a70542492d4040b88256605c81bc2d1
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2019-03-16 19:10:42 +0100

    #1: Implement basic setup for integration tests

--- tests/Integration/Suites/LizardsAndPumpkins/Messaging/Queue/Sqs/QueueAwsTest.php
+++ tests/Integration/Suites/LizardsAndPumpkins/Messaging/Queue/Sqs/QueueAwsTest.php
@@ -1,12 +1,59 @@
 <?php
 declare(strict_types=1);
 
+namespace LizardsAndPumpkins\Messaging\Queue\Sqs;
+
+use Aws\Sqs\SqsClient;
+use Guzzle\Service\Resource\Model;
 use PHPUnit\Framework\TestCase;
 
 class QueueAwsTest extends TestCase
 {
+    /**
+     * @var SqsQueue
+     */
+    private $queue;
+
+    /**
+     * @var SqsClient
+     */
+    private $sqsClient;
+
+    /**
+     * @var string
+     */
+    private $queueUrl;
+
+    protected function setUp()
+    {
+        $sqsClient = SqsClient::factory([
+            'credentials' => [
+                'key' => $_ENV['aws_key'],
+                'secret' => $_ENV['aws_secret'],
+            ],
+            'region' => 'eu-central-1',
+        ]);
+        $queueName = str_replace('.', '-', uniqid('lap-sqs-test', true));
+        $this->sqsClient = $sqsClient;
+
+        /** @var Model $response */
+        $response = $this->sqsClient->createQueue([
+            'QueueName' => $queueName,
+        ]);
+        $this->queueUrl = $response->get('QueueUrl');
+        $this->queue = new SqsQueue($this->sqsClient, $queueName);
+    }
+
+    protected function tearDown()
+    {
+        $this->sqsClient->deleteQueue([
+            'QueueUrl' => $this->queueUrl,
+        ]);
+    }
+
+
     public function testFalse()
     {
-        $this->assertFalse(true);
+
     }
 }
