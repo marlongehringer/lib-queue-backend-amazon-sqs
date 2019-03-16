34fa4b64c4a1e6cf18284945c3f9e64343ed3089
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2019-03-16 19:19:53 +0100

    #1: integration test for message return

--- tests/Integration/Suites/LizardsAndPumpkins/Messaging/Queue/Sqs/QueueAwsTest.php
+++ tests/Integration/Suites/LizardsAndPumpkins/Messaging/Queue/Sqs/QueueAwsTest.php
@@ -5,6 +5,7 @@ namespace LizardsAndPumpkins\Messaging\Queue\Sqs;
 
 use Aws\Sqs\SqsClient;
 use Guzzle\Service\Resource\Model;
+use LizardsAndPumpkins\Messaging\MessageReceiver;
 use LizardsAndPumpkins\Messaging\Queue\Message;
 use PHPUnit\Framework\TestCase;
 
@@ -61,5 +62,30 @@ class QueueAwsTest extends TestCase
         $message = Message::withCurrentTime('my Message', [], []);
         $this->queue->add($message);
         $this->assertSame(1, $this->count());
+    }
+
+    public function testAddedMessageComesBack()
+    {
+        $message = Message::withCurrentTime('my Message', ['with complex payload'], ['and' => 'some metadata']);
+        $this->queue->add($message);
+
+        $receiver = new class implements MessageReceiver
+        {
+            /**
+             * @var Message
+             */
+            public $message;
+
+            public function receive(Message $message)
+            {
+                $this->message = $message;
+            }
+        };
+
+        $this->queue->consume($receiver, 1);
+
+        /** @var Message $returnedMessage */
+        $returnedMessage = $receiver->message;
+        $this->assertSame($message->serialize(), $returnedMessage->serialize());
     }
 }
